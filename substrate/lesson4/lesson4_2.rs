10f94292db751917abf429a9f20a699126a0512e
moerben2021 <101541008+moerben2021@users.noreply.github.com>
2022-07-16 22:11:32 +0800

    Update lesson4_2.rs

--- substrate/lesson4/lesson4_2.rs
+++ substrate/lesson4/lesson4_2.rs
@@ -1,8 +1,15 @@
-1
+example1:
 fn main() {
     let s1 = String::from("hello");
     let s2 = s1.clone();
     println!("s1 = {}, s2 = {}", s1, s2);
+}
+
+example2:
+fn main() {
+    let x = vec![1,2];
+    let y = x;
+    println!("y: {:?}",y);
 }
 
 
