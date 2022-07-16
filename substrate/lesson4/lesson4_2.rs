d85ab7bca891ba00ab5d335c8ea672e9ec83057b
moerben2021 <101541008+moerben2021@users.noreply.github.com>
2022-07-16 22:25:01 +0800

    Update lesson4_2.rs

--- substrate/lesson4/lesson4_2.rs
+++ substrate/lesson4/lesson4_2.rs
@@ -13,4 +13,23 @@ fn main() {
     println!("y: {:?}",y);
 }
 
+example3:
+fn main() {
+    let my_string = String::from("hello world");
+    let word = first_word(&my_string[..]);
+    println!("result: {}", word);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    
+    for( i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    
+    &s[..]
+}
+
 
